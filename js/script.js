189980d264e4275afd7323cb446a86d5a4b74a8f
Tracy Tuhakaraina <tracytmail@gmail.com>
2024-12-06 22:48:02 +1100

    Added dropdown list to change number of profiles displayed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("select");
 
-
-const getData = async function () {
-    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function (numUsers) {
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json();
 
     const userResults = data.results; //array of objects
@@ -24,4 +24,9 @@ const displayUsers = function (userResults) {
     }
 };
 
-getData();
+selectUserNumber.addEventListener("change", function () {
+    const numUsers = selectUserNumber.value;
+    getData(numUsers);
+});
+
+getData(1);
